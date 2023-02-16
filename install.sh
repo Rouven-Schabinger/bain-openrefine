f7cb889176b18435a6892876e78af5ddc7b3b4db
Felix Lohmeier <mail@felixlohmeier.de>
2023-02-16 11:12:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-wget -q -O openrefine.tar.gz "https://oss.sonatype.org/service/local/artifact/maven/content?r=releases&g=org.openrefine&a=openrefine&v=3.7.0&c=linux&p=tar.gz"
+wget -q -O openrefine.tar.gz "https://oss.sonatype.org/service/local/artifact/maven/content?r=releases&g=org.openrefine&a=openrefine&v=3.6.2&c=linux&p=tar.gz"
 tar -xzf openrefine.tar.gz
 rm openrefine.tar.gz
