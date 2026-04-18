2adf7c8229e4798f93b41c8d7f686a42f187957b
Rouven-Schabinger <69207502+Rouven-Schabinger@users.noreply.github.com>
2026-04-18 13:32:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-wget -q -O openrefine.tar.gz "https://github.com/OpenRefine/OpenRefine/releases/download/3.8.7/openrefine-linux-3.8.7.tar.gz"
+wget -q -O openrefine.tar.gz "https://github.com/OpenRefine/OpenRefine/releases/download/3.8.7/openrefine-linux-3.10.1.tar.gz"
 tar -xzf openrefine.tar.gz
 rm openrefine.tar.gz
 gh codespace ports forward 3333
